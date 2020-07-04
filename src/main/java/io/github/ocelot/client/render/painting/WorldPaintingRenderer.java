761469fc650c5ec85ae83e09145274b216e15694
Ocelot <FinntheRaider@gmail.com>
2020-07-03 17:56:55 -0700

    Started working on ripple effect

--- src/main/java/io/github/ocelot/client/render/painting/WorldPaintingRenderer.java
+++ src/main/java/io/github/ocelot/client/render/painting/WorldPaintingRenderer.java
@@ -1,20 +1,21 @@
 package io.github.ocelot.client.render.painting;
 
-import com.google.gson.JsonSyntaxException;
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
+import io.github.ocelot.WorldPainter;
+import io.github.ocelot.client.ShapeRenderer;
 import io.github.ocelot.client.framebuffer.AdvancedFbo;
 import io.github.ocelot.painting.Painting;
-import net.minecraft.client.MainWindow;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.Matrix3f;
 import net.minecraft.client.renderer.Matrix4f;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.client.renderer.texture.PaintingSpriteUploader;
+import net.minecraft.client.renderer.texture.Texture;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.shader.ShaderGroup;
+import net.minecraft.util.LazyValue;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -22,7 +23,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nullable;
-import java.io.IOException;
+
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
 
 /**
  * <p>Manages the rendering of {@link Painting} onto a painting frame.</p>
@@ -34,49 +37,15 @@ public class WorldPaintingRenderer
 {
     private static final Logger LOGGER = LogManager.getLogger();
     private static final ResourceLocation SHADER_LOCATION = new ResourceLocation("shaders/post/wobble.json");
+    private static final ResourceLocation FBO_LOCATION = new ResourceLocation(WorldPainter.MOD_ID, "textures/entity/painting_fbo.png");
 
-    private static AdvancedFbo fbo;
-    private static ShaderGroup rippleShader;
-    private static boolean useShader;
-    private static boolean drawing;
+    private static final LazyValue<AdvancedFbo> FRAMEBUFFER = new LazyValue<>(WorldPaintingRenderer::createFbo);
 
-    private static void createShader()
+    private static AdvancedFbo createFbo()
     {
-        Minecraft minecraft = Minecraft.getInstance();
-        try
-        {
-            rippleShader = new ShaderGroup(minecraft.getTextureManager(), minecraft.getResourceManager(), fbo.getVanillaWrapper(), SHADER_LOCATION);
-            rippleShader.createBindFramebuffers(minecraft.getMainWindow().getFramebufferWidth(), minecraft.getMainWindow().getFramebufferHeight());
-            useShader = true;
-        }
-        catch (IOException | JsonSyntaxException e)
-        {
-            LOGGER.warn("Failed to load shader: {}", SHADER_LOCATION, e);
-            useShader = false;
-        }
-    }
-
-    private static void updateFbo()
-    {
-        MainWindow window = Minecraft.getInstance().getMainWindow();
-        if (fbo == null)
-            fbo = createFbo(window.getFramebufferWidth(), window.getFramebufferHeight());
-
-        if (rippleShader == null)
-            createShader();
-
-        if (fbo.getWidth() != window.getFramebufferWidth() || fbo.getHeight() != window.getFramebufferHeight())
-        {
-            fbo.close();
-            fbo = createFbo(window.getFramebufferWidth(), window.getFramebufferHeight());
-            rippleShader.createBindFramebuffers(window.getFramebufferWidth(), window.getFramebufferHeight());
-        }
-    }
-
-    private static AdvancedFbo createFbo(int width, int height)
-    {
-        AdvancedFbo fbo = new AdvancedFbo.Builder(width, height).addColorTextureBuffer().setDepthRenderBuffer().build();
+        AdvancedFbo fbo = new AdvancedFbo.Builder(16, 16).addColorTextureBuffer().build();
         fbo.create();
+        Minecraft.getInstance().getTextureManager().loadTexture(FBO_LOCATION, (Texture) fbo.getColorTextureAttachment(0));
         return fbo;
     }
 
@@ -136,9 +105,45 @@ public class WorldPaintingRenderer
                 pos(matrix4f, matrix3f, builder, f16, f17, f11, f13, 0.5F, 1, 0, 0, combinedLight);
                 Minecraft.getInstance().getRenderTypeBuffers().getBufferSource().finish(RenderType.getEntitySolid(backSprite.getAtlasTexture().getTextureLocation()));
 
-                IVertexBuilder frontBuilder = buffer.getBuffer(RenderType.getEntitySolid(WorldPaintingTextureCache.getTexture(painting)));
-                renderFront(matrix4f, matrix3f, frontBuilder, f15, f16, f17, f18, i, j, d0, d1, k, l, combinedLight);
-                Minecraft.getInstance().getRenderTypeBuffers().getBufferSource().finish(RenderType.getEntitySolid(WorldPaintingTextureCache.getTexture(painting)));
+                if (!renderEffects)
+                {
+                    IVertexBuilder frontBuilder = buffer.getBuffer(RenderType.getEntitySolid(WorldPaintingTextureCache.getTexture(painting)));
+                    renderFront(matrix4f, matrix3f, frontBuilder, f15, f16, f17, f18, i, j, d0, d1, k, l, combinedLight);
+                    Minecraft.getInstance().getRenderTypeBuffers().getBufferSource().finish(RenderType.getEntitySolid(WorldPaintingTextureCache.getTexture(painting)));
+                }
+                else
+                {
+                    AdvancedFbo fbo = FRAMEBUFFER.getValue();
+                    fbo.bind(true);
+                    {
+                        fbo.clear();
+
+                        RenderSystem.matrixMode(GL_PROJECTION);
+                        RenderSystem.pushMatrix();
+                        RenderSystem.loadIdentity();
+                        RenderSystem.ortho(0, 1, 0, 1, 0.3, 1000.0);
+                        RenderSystem.matrixMode(GL_MODELVIEW);
+                        RenderSystem.pushMatrix();
+                        RenderSystem.loadIdentity();
+                        RenderSystem.translatef(0, 0, -10);
+
+                        Minecraft.getInstance().getTextureManager().bindTexture(WorldPaintingTextureCache.getTexture(painting));
+                        ShapeRenderer.drawRectWithTexture(0, 0, k, l, 1, 1, -1,-1,i,j);
+
+                        RenderSystem.matrixMode(GL_PROJECTION);
+                        RenderSystem.popMatrix();
+                        RenderSystem.matrixMode(GL_MODELVIEW);
+                        RenderSystem.popMatrix();
+                    }
+                    Minecraft.getInstance().getFramebuffer().bindFramebuffer(true);
+
+                    IVertexBuilder fboBuilder = buffer.getBuffer(RenderType.getEntitySolid(FBO_LOCATION));
+                    pos(matrix4f, matrix3f, fboBuilder, f15, f18, 1, 0, -0.5F, 0, 0, -1, combinedLight);
+                    pos(matrix4f, matrix3f, fboBuilder, f16, f18, 0, 0, -0.5F, 0, 0, -1, combinedLight);
+                    pos(matrix4f, matrix3f, fboBuilder, f16, f17, 0, 1, -0.5F, 0, 0, -1, combinedLight);
+                    pos(matrix4f, matrix3f, fboBuilder, f15, f17, 1, 1, -0.5F, 0, 0, -1, combinedLight);
+                    Minecraft.getInstance().getRenderTypeBuffers().getBufferSource().finish(RenderType.getEntitySolid(FBO_LOCATION));
+                }
 
                 if (renderBorder && (painting == null || painting.hasBorder()))
                 {
@@ -150,7 +155,7 @@ public class WorldPaintingRenderer
         }
     }
 
-    private static void renderFront(Matrix4f matrix4f, Matrix3f matrix3f, IVertexBuilder builder, float f15, float f16, float f17, float f18, int i, int j, double d0, double d1, int k, int l, int combinedLight)
+    private static void renderFront(@Nullable Matrix4f matrix4f, @Nullable Matrix3f matrix3f, IVertexBuilder builder, float f15, float f16, float f17, float f18, int i, int j, double d0, double d1, int k, int l, int combinedLight)
     {
         float f19 = (float) (0.0625 * (d0 * (i - k)));
         float f20 = (float) (0.0625 * (d0 * (double) (i - (k + 1))));
@@ -162,9 +167,29 @@ public class WorldPaintingRenderer
         pos(matrix4f, matrix3f, builder, f15, f17, f20, f22, -0.5F, 0, 0, -1, combinedLight);
     }
 
-    private static void pos(Matrix4f matrix, Matrix3f normal, IVertexBuilder buffer, float x, float y, float u, float v, float z, int normalX, int normalY, int normalZ, int combinedLight)
+    private static void pos(@Nullable Matrix4f matrix, @Nullable Matrix3f normal, IVertexBuilder buffer, float x, float y, float u, float v, float z, int normalX, int normalY, int normalZ, int combinedLight)
     {
-        buffer.pos(matrix, x, y, z).color(255, 255, 255, 255).tex(u, v).overlay(OverlayTexture.NO_OVERLAY).lightmap(combinedLight).normal(normal, (float) normalX, (float) normalY, (float) normalZ).endVertex();
+        if (matrix != null)
+        {
+            buffer.pos(matrix, x, y, z);
+        }
+        else
+        {
+            buffer.pos(x, y, z);
+        }
+
+        buffer.color(255, 255, 255, 255).tex(u, v).overlay(OverlayTexture.NO_OVERLAY).lightmap(combinedLight);
+
+        if (normal != null)
+        {
+            buffer.normal(normal, (float) normalX, (float) normalY, (float) normalZ);
+        }
+        else
+        {
+            buffer.normal((float) normalX, (float) normalY, (float) normalZ);
+        }
+
+        buffer.endVertex();
     }
 
     /**
@@ -179,9 +204,6 @@ public class WorldPaintingRenderer
      */
     public static void renderPainting(MatrixStack stack, IRenderTypeBuffer buffer, @Nullable Painting painting, boolean renderBorder, boolean renderEffects, int combinedLight)
     {
-        PaintingSpriteUploader paintingspriteuploader = Minecraft.getInstance().getPaintingSpriteUploader();
-        TextureAtlasSprite backSprite = paintingspriteuploader.getBackSprite();
-        updateFbo();
-        renderBackground(stack, buffer, painting, renderBorder, renderEffects, backSprite, combinedLight);
+        renderBackground(stack, buffer, painting, renderBorder, renderEffects, Minecraft.getInstance().getPaintingSpriteUploader().getBackSprite(), combinedLight);
     }
 }
